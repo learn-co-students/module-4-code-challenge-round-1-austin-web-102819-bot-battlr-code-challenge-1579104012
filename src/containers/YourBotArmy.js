41e90dfb83f3e80c85c365a48d9d730902aa8ae4
rachfuerst <rach.fuerst@gmail.com>
2020-01-15 11:39:11 -0600

    Cleaning up removeRobot function

--- src/containers/YourBotArmy.js
+++ src/containers/YourBotArmy.js
@@ -10,7 +10,7 @@ class YourBotArmy extends React.Component {
       <div className="ui segment inverted olive bot-army">
         <div className="ui five column grid">
           <div className="row bot-army-row">
-            {botArmy.map((bot, i) => {
+            {this.props.botArmy.map((bot, i) => {
             return <BotCard key={i} bot={bot} addToArmy={this.props.addToArmy} removeFromArmy={this.props.removeFromArmy}/>;
           })}
             Your Bot Army
