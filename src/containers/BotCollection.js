5bcd2499d9db0618ba23e6b070f41e2371a8a94d
Heidi Boulter <boulter.heidi@gmail.com>
2020-01-15 11:05:56 -0600

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,24 @@
 import React from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs";
 
 class BotCollection extends React.Component {
-  //your code here
+  //   your code here
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  render() {
+    return (
+      <div className="ui four column grid">
+        <div className="row"></div>
+        {this.props.botData.map(bot => (
+          <BotCard
+            key={bot.id}
+            bot={bot}
+            handleBotClick={this.props.handleBotClick}
+          />
+        ))}
+      </div>
+    );
   }
-
-};
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,45 @@
 import React from "react";
+import BotsCollection from "./BotCollection";
+import YourBotArmy from "./YourBotArmy";
+
+const API = "https://bot-battler-api.herokuapp.com/api/v1/bots";
 
 class BotsPage extends React.Component {
   //start here with your code for step one
+  constructor() {
+    super();
+    this.state = {
+      botData: [],
+      botArmy: []
+    };
+  }
+
+  componentDidMount() {
+    fetch(API)
+      .then(response => response.json())
+      .then(data =>
+        this.setState({ botData: data }, () => console.log(this.state.botData))
+      );
+  }
+
+  handleBotClick = bot => {
+    this.setState({
+      botArmy: [...this.state.botArmy, bot]
+    });
+    console.log("clicked");
+  };
 
   render() {
     return (
       <div>
-        {/* put your components here */}
+        <YourBotArmy botArmy={this.state.botArmy} />
+        <BotsCollection
+          botData={this.state.botData}
+          handleBotClick={this.handleBotClick}
+        />
       </div>
     );
   }
-
 }
 
 export default BotsPage;
