a1cfe400269c88c7d7dd30f069dca4630ce5e21a
Faith <faithe1937@gmail.com>
2020-01-15 12:04:07 -0600

    submission for deadline

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,51 @@
-import React from "react";
+import React, { Component } from "react";
 import BotCard from "../components/BotCard";
+import BotSpecs from "../components/BotSpecs";
 
-class BotCollection extends React.Component {
-  //your code here
+class BotCollection extends Component {
+  state = {
+    currentBot: {}
+  };
 
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
+  //toggle state to remove from army list
+  showBot = bot => {
+    this.setState({
+      currentBot: bot
+    });
+  };
+
+  goBack = () => {
+    this.setState({
+      currentBot: {}
+    });
+  };
+
+  renderBots = () => {
+    if (!this.state.currentBot.id) {
+      return (
+        <div className="row">
+          Collection of all bots
+          {this.props.bots.map(bot => (
+            <BotCard key={bot.id} bot={bot} showBot={this.showBot} />
+          ))}
+        </div>
+      );
+
+    } else {
+      return (
+        <BotSpecs
+          bot={this.state.currentBot}
+          enlist={this.props.enlistBot}
+          goBack={this.goBack}
+          enlistBot={this.props.enlistBot}
+        />
+      );
+    }
+  };
+
+  render() {
+    return <div className="ui four column grid">{this.renderBots()}</div>;
   }
-
-};
+}
 
 export default BotCollection;
