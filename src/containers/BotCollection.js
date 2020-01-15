6bfec7c96df9be66cda8e19bb291cc8ab7b80606
Yining Wang <yining.wang@utexas.edu>
2020-01-15 11:06:53 -0600

    checkpoint

--- src/containers/BotCollection.js
+++ src/containers/BotCollection.js
@@ -1,20 +1,23 @@
-import React from "react";
-import BotCard from "../components/BotCard";
+import React from 'react';
+import BotCard from '../components/BotCard';
 
 class BotCollection extends React.Component {
-  //your code here
-
-  render(){
-  	return (
-  	  <div className="ui four column grid">
-    		<div className="row">
-    		  {/*...and here..*/}
-    		  Collection of all bots
-    		</div>
-  	  </div>
-  	);
-  }
-
-};
+	render() {
+		return (
+			<div className="ui four column grid">
+				<div className="row">
+					{this.props.allBots.map((bot) => (
+						<BotCard
+							addToArmy={() => this.props.addToArmy(bot)}
+							bot={bot}
+							container={'collection'}
+						></BotCard>
+					))}
+					Collection of all bots
+				</div>
+			</div>
+		);
+	}
+}
 
 export default BotCollection;
--- src/containers/BotsPage.js
+++ src/containers/BotsPage.js
@@ -1,16 +1,60 @@
-import React from "react";
+import React from 'react';
+import YourBotArmy from './YourBotArmy';
+import BotCollection from './BotCollection';
 
 class BotsPage extends React.Component {
-  //start here with your code for step one
+	constructor() {
+		super();
+		this.state = {
+			allBots: [],
+			army: []
+		};
+	}
+	//start here with your code for step one
 
-  render() {
-    return (
-      <div>
-        {/* put your components here */}
-      </div>
-    );
-  }
+	componentDidMount() {
+		fetch('https://bot-battler-api.herokuapp.com/api/v1/bots')
+			.then((res) => res.json())
+			.then((json) => this.setState({ allBots: json }));
+	}
 
+	addToArmy = (bot) => {
+		if (!this.state.army.includes(bot)) {
+			this.setState((previousState) => {
+				return {
+					allBots: previousState.allBots,
+					army: [...previousState.army, bot]
+				};
+			});
+		}
+	};
+
+	deleteFromArmy = (bot) => {
+		let currentArmy = this.state.army;
+		let botIndex = currentArmy.indexOf(bot);
+		let deleted = currentArmy.splice(botIndex, 1);
+		this.setState((previousState) => {
+			return {
+				allBots: previousState.allBots,
+				army: currentArmy
+			};
+		});
+	};
+
+	render() {
+		return (
+			<div>
+				<YourBotArmy
+					army={this.state.army}
+					deleteFromArmy={this.deleteFromArmy}
+				></YourBotArmy>
+				<BotCollection
+					allBots={this.state.allBots}
+					addToArmy={this.addToArmy}
+				></BotCollection>
+			</div>
+		);
+	}
 }
 
 export default BotsPage;
